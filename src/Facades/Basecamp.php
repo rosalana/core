b206c68eda33da960e5423db99b16c9c58f95e53
Michael Bany <mobany526@gmail.com>
2025-02-01 23:36:11 +0100

    docs for basecamp facade

--- src/Facades/Basecamp.php
+++ src/Facades/Basecamp.php
@@ -4,6 +4,12 @@ namespace Rosalana\Core\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Rosalana\Core\Services\Basecamp\AppsService apps()
+ * @method static \Rosalana\Accounts\Services\Basecamp\UsersService users()
+ * 
+ * @see \Rosalana\Core\Services\Basecamp\Manager
+ */
 class Basecamp extends Facade
 {
     protected static function getFacadeAccessor()
