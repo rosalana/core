fc17989ec361c7955c1c573600da29222ee6c024
Michael Bany <mobany526@gmail.com>
2025-04-03 17:51:07 +0200

    s

--- src/Providers/Core.php
+++ src/Providers/Core.php
@@ -27,15 +27,17 @@ class Core implements Package
                         '--tag' => "rosalana-config"
                     ]);
 
-                    RosalanaConfig::new('basecamp')
+                    $config = RosalanaConfig::read();
+
+                    $config::new('basecamp')
                         ->add('url', "env('ROSALANA_BASECAMP_URL', 'http://localhost:8000')")
                         ->add('secret', "env('ROSALANA_APP_SECRET', 'secret')")
                         ->comment('Here you can define the settings for the Rosalana Auth. This settings are used for authorizate your app to the Rosalana Basecamp to use Basecamp services.', 'Rosalana Basecamp Auth Settings');
 
-                    RosalanaConfig::new('installed')
+                    $config::new('installed')
                         ->comment('All installed Rosalana packages. This array is used to determine if the package has been installed or not and with the correct version. DO NOT MODIFY THIS ARRAY MANUALLY!', 'Rosalana Core Installed');
 
-                    RosalanaConfig::save();
+                    $config::save();
                 }
             ],
             'env' => [
