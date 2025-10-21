ac4122c5b4c9122744ea7366233138120dde6bf6
Michael Bany <mobany526@gmail.com>
2025-10-21 17:27:07 +0200

    feat: scaleton of Revizor manager

---
+++ src/Services/Revizor/Manager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rosalana\Core\Services\Revizor;
+
+use Rosalana\Core\Support\Cipher;
+
+class Manager
+{
+    public function __construct(
+        protected TicketManager $ticketManager
+    ) {}
+
+    public function ticket(array|string|null $ticket = null): Ticket|TicketManager
+    {
+        if ($ticket === null) {
+            return $this->ticketManager;
+        } else {
+            return Ticket::make($ticket);
+        }
+    }
+    /**
+     * TicketManager je pro Find ticket nebo create a vrací Ticket
+     * Ticket je samotný Ticket
+     * TicketManager může provádět i validace
+     */
+
+    public function request()
+    {
+        // sign request TODO
+    }
+
+    public function hide($value)
+    {
+        return Cipher::encrypt($value);
+    }
+
+    public function reveal($value)
+    {
+        return Cipher::decrypt($value);
+    }
+}
