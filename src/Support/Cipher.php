8f14f3cc423c7b419da771c224df83e041908d47
Michael Bany <mobany526@gmail.com>
2025-11-15 17:55:38 +0100

    refactor: update method documentation and rename wrapToString to wrap for clarity

--- src/Support/Cipher.php
+++ src/Support/Cipher.php
@@ -6,6 +6,10 @@ use Rosalana\Core\Facades\App;
 
 class Cipher
 {
+
+    /**
+     * Encrypt the given value by secret_key to make it safe.
+     */
     public static function encrypt(string $value): string
     {
         $data = base64_decode($value);
@@ -19,6 +23,9 @@ class Cipher
         return openssl_decrypt($ciphertext, 'AES-256-CBC', $key, 0, $iv);
     }
 
+    /**
+     * Decrypt the given value by secret_key to retrieve original data.
+     */
     public static function decrypt(string $value): string
     {
         $key = substr(hash('sha256', self::getSecretKey(), true), 0, 32);
@@ -29,12 +36,18 @@ class Cipher
         return base64_encode($iv . $encrypted);
     }
 
-    public static function wrapToString(array $data): string
+    /**
+     * Wrap data into a string for transport.
+     */
+    public static function wrap(array $data): string
     {
         return base64_encode(json_encode($data, JSON_UNESCAPED_SLASHES));
     }
 
-    public static function unwrapString(string $data): array
+    /**
+     * Unwrap data from a transported string to array.
+     */
+    public static function unwrap(string $data): array
     {
         $decoded = base64_decode($data);
         return json_decode($decoded, true) ?? [];
