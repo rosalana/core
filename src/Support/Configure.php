919bfcf383e7b66306fdbea86c6f4b3a6632cb29
Michael Bany <mobany526@gmail.com>
2025-11-24 21:18:02 +0100

    fix(Configure): refactor node handling and enhance comment creation logic

--- src/Support/Configure.php
+++ src/Support/Configure.php
@@ -3,8 +3,11 @@
 namespace Rosalana\Core\Support;
 
 use Illuminate\Support\Collection;
+use Rosalana\Core\Contracts\Configure\Node;
+use Rosalana\Core\Support\Configure\Node\RichComment;
+use Rosalana\Core\Support\Configure\Node\Section;
+use Rosalana\Core\Support\Configure\Node\Value;
 use Rosalana\Core\Support\Configure\Reader;
-use Rosalana\Core\Support\Configure\Section;
 use Rosalana\Core\Support\Configure\Writer;
 
 class Configure
@@ -30,14 +33,9 @@ class Configure
 
     protected function read(): self
     {
-        $this->nodes = $this->reader->read()
-            ->each(fn($node) => $node->setParent($this));
+        $this->reader->read()
+            ->each(fn($node) => $this->addNode($node));
 
-        return $this;
-    }
-
-    public function section(string $name): self
-    {
         return $this;
     }
 
@@ -46,9 +44,65 @@ class Configure
         return $this->nodes;
     }
 
-
-    protected function activate(string $name): Section
+    public function addNode(Node $node): self
     {
-        return new Section();
+        $this->nodes->push($node->setParent($this));
+
+        return $this;
+    }
+
+    public function section(string $path): Node
+    {
+        return new Section(0, 0, []);
+    }
+
+    public function value(string $path): Node
+    {
+        return new Value(0, 0, [], 's', 's');
+    }
+
+    /**
+     * Create a rich or simple comment node.
+     * If description is provided, a rich comment is created.
+     * Otherwise, a simple comment is created.
+     */
+    public function comment(string $label, ?string $description = null): Node
+    {
+        if ($description) {
+            return new RichComment(0, 0, [], $label, $description);
+        }
+
+        return new RichComment(0, 0, [], $label, null); // for now
+    }
+
+    public function add(string $path, string $value): Node
+    {
+        return new Value(0, 0, [], '', '');
+    }
+
+    public function set(string $path, string $value): Node
+    {
+        return new Value(0, 0, [], '', '');
+    }
+
+    public function remove(string $path): self
+    {
+        return $this;
+    }
+
+    public function save(): void
+    {
+        //
+    }
+
+    protected function findParent(string $path): Node|self
+    {
+        return $this;
+    }
+
+
+    public function toArray(): array
+    {
+        return $this->nodes->map(fn($node) => $node->toArray())->toArray();
     }
 }
