7db42c2bade38e80f70bdfcefd6d006c5c3f70e7
Michael Bany <mobany526@gmail.com>
2025-12-31 20:43:04 +0100

    fix(Outpost): update method return types to use Promise instead of void

--- src/Facades/Outpost.php
+++ src/Facades/Outpost.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Rosalana\Core\Services\Outpost\Manager except(string|array $name)
  * @method static \Rosalana\Core\Services\Outpost\Manager broadcast()
  * @method static \Rosalana\Core\Services\Outpost\Manager responseTo(\Rosalana\Core\Services\Outpost\Message $message)
- * @method static void request(?string $name = null, array $payload = [])
- * @method static void confirm(?string $name = null, array $payload = [])
- * @method static void fail(?string $name = null, array $payload = [])
- * @method static void unreachable(?string $name = null, array $payload = [])
+ * @method static \Rosalana\Core\Services\Outpost\Promise request(?string $name = null, array $payload = [])
+ * @method static \Rosalana\Core\Services\Outpost\Promise confirm(?string $name = null, array $payload = [])
+ * @method static \Rosalana\Core\Services\Outpost\Promise fail(?string $name = null, array $payload = [])
+ * @method static \Rosalana\Core\Services\Outpost\Promise unreachable(?string $name = null, array $payload = [])
  * @method static \Rosalana\Core\Services\Outpost\Manager reset()
  * 
  * @see \Rosalana\Core\Services\Outpost\Manager
