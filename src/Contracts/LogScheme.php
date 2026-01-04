8b9ba3d1071ba58fe0a33d18d4fc8780d361567a
Michael Bany <mobany526@gmail.com>
2026-01-04 18:31:13 +0100

    refactor(LogScheme): remove Trace parameter from methods for simplified interface

--- src/Contracts/LogScheme.php
+++ src/Contracts/LogScheme.php
@@ -3,47 +3,34 @@
 namespace Rosalana\Core\Contracts;
 
 use Rosalana\Core\Services\Logging\LogEntry;
-use Rosalana\Core\Services\Trace\Trace;
 
 interface LogScheme
 {
     /**
-     * Determine if the log scheme matches the given trace.
-     * 
-     * @param Trace $trace
-     * @return bool
-     */
-    public function match(Trace $trace): bool;
-
-    /**
      * Format the given trace into log entries.
      * 
-     * @param Trace $trace
      * @return void
      */
-    public function format(Trace $trace): void;
+    public function format(): void;
 
     /**
      * Format exception traces into log entries.
      * 
-     * @param Trace $trace
      * @return void
      */
-    public function formatException(Trace $trace): void;
+    public function formatException(): void;
 
     /**
      * Build data from the formatted trace.
      * 
-     * @param Trace $trace
      * @return LogEntry[]
      */
-    public function build(Trace $trace): iterable;
+    public function build(): array;
 
     /**
      * Build data from the formatted exception trace.
      * 
-     * @param Trace $trace
      * @return LogEntry[]
      */
-    public function buildException(Trace $trace): iterable;
+    public function buildException(): array;
 }
