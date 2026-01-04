1d97a96612a387bbf3b5481b5bd513954b586e5f
Michael Bany <mobany526@gmail.com>
2026-01-04 20:36:26 +0100

    feat(Schemes): schemes waiting for implemention

---
+++ src/Logging/Schemes/OutpostHandlerScheme.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rosalana\Core\Logging\Schemes;
+
+use Rosalana\Core\Services\Logging\LogScheme;
+
+class OutpostHandlerScheme extends LogScheme
+{
+    public function format(): void
+    {
+        //
+    }
+
+    public function formatException(): void
+    {
+        //
+    }
+}
