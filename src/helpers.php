5e66738af1ede049c2e4a10794bed4201a2ccde4
Michael Bany <mobany526@gmail.com>
2026-01-10 16:23:32 +0100

    feat: action helper function to create inline action class instance

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,19 @@
 <?php
 
 use Rosalana\Core\Contracts\Action;
+use Rosalana\Core\Services\Actions\Inline;
 use Rosalana\Core\Services\Actions\Runner;
 
 if (!function_exists('run')) {
     function run(Action $action): mixed
     {
         return Runner::run($action);
+    }
+}
+
+if (!function_exists('action')) {
+    function action(\Closure $callback): Action
+    {
+        return Inline::make($callback);
     }
 }
